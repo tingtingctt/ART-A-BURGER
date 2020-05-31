2cf66bf35277f1a14844462a4868e9d793ff45a0
Tingting Chen <tingtingmacpro@Tingtings-MacBook-Pro.local>
2020-05-30 21:26:15 -0700

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers (name) VALUES ('MICHELANGELLO BURGER');
+INSERT INTO burgers (name) VALUES ('CLAUDE MONETIZE BURGER');
+INSERT INTO burgers (name, devoured) VALUES ('DO NOT TRUMP BURGER', true);
+INSERT INTO burgers (name, devoured) VALUES ('PABLANO PICASSO BURGER', true);
+INSERT INTO burgers (name, devoured) VALUES ('LEONARDO DA VINCHEESE BURGER', true);
+INSERT INTO burgers (name) VALUES ('VINCENT VAN GOTTA BURGER');
